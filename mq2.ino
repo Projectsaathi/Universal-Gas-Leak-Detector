6a99bf8e69d54d96caef1914463febec052d698c
projectsaathi01 <projectsaathi@gmail.com>
2019-11-18 09:04:56 +0545

    logic changed

--- mq2.ino
+++ mq2.ino
@@ -1,13 +1,26 @@
+// This is sample code , you can change according to your logic.
 int led_pin = 13;
+int siren_pin = 12;
 int sensor_pin = A0;
 int value = 200;
 void setup() {
 Serial.begin(9600);
 pinMode(led_pin, OUTPUT);
+
 pinMode(sensor_pin, INPUT);
 }
 void loop() {
- int sensor_pin  = analogRead(sensor_pin);
+ int sensor_value  = analogRead(sensor_pin);
 Serial.println(sensor_pin);
-delay(1000);
+if(sensor_value > 30)
+{
+  digitalWrite(led_pin, HIGH);
+  digitalWrite(siren_pin, HIGH);
+}
+
+else
+{
+  digitalWrite(led_pin, LOW);
+  digitalWrite(sensor_pin, LOW);
+}
 }
